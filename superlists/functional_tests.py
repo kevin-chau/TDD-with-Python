1cca4254ea743320749ebfc5e786490aa57da966
kevinchau321 <kevinchau321@gmail.com>
2016-11-24 23:39:11 -0800

    using unit tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,37 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-# check for Django welcome page
-# browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-# She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-# She is invited to enter a to-do item straight away
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+        # She is invited to enter a to-do item straight away
+        [...rest of comments as before]
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
-
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
-# The page updates again, and now shows both items on her list
-
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
-
-# She visits that URL - her to-do list is still there.
-
-# Satisfied, she goes back to sleep
-
-browser.quit()
+if __name__ == '__main__':
+    unittest.main(warnings='ignore') 
